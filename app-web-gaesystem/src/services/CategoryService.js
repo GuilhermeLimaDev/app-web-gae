a3e4c8bc424be220ea76431cffddb3df553fe952
Guilherme-Script <guilhermeguiguigugu2509@gmail.com>
2025-08-05 19:43:24 +0000

    criando serviço de Categoria

---
+++ app-web-gaesystem/src/services/CategoryService.js
@@ -0,0 +1,11 @@
+import http from "../connection/Connection";
+const route = "category"; //producao
+const teste = "categorias.json"; //desenvolvimento com lista local
+
+const getCategorys = () => {
+  return http.get(teste);
+};
+
+const CategoryService = { getCategorys };
+
+export default CategoryService;
