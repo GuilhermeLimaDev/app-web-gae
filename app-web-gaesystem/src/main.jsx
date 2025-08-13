0d15fc67de249b7883f70b6ea63d2bf42b91db90
Guilherme-Script <guilhermeguiguigugu2509@gmail.com>
2025-08-13 14:32:28 +0000

    envolvendo projeto com o provinder do UserContext

--- app-web-gaesystem/src/main.jsx
+++ app-web-gaesystem/src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import AppRoutes from './pages/approutes/AppRoutes'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./pages/approutes/AppRoutes";
+import { UserProvider } from "./context/UserContext";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
-  </StrictMode>,
-)
-
+    <UserProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </UserProvider>
+  </StrictMode>
+);
