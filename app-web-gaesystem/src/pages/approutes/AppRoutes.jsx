1c8bace2f0cd2caf71d6f09464a11581a98de58e
Guilherme-Script <guilhermeguiguigugu2509@gmail.com>
2025-08-13 14:33:58 +0000

    configurando rotas do projeto

--- app-web-gaesystem/src/pages/approutes/AppRoutes.jsx
+++ app-web-gaesystem/src/pages/approutes/AppRoutes.jsx
@@ -3,15 +3,18 @@ import Homepage from "../homepage/Homepage";
 import Reports from "../reports/Reports";
 import ProblemDetails from "../problemdetails/ProblemDetails";
 import Header from "../../components/header/Header";
+import LoginForm from "../login/LoginForm";
 
 const AppRoutes = () => {
   return (
     <>
       <Header />
       <Routes>
+        <Route path="/" element={<LoginForm />} />
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/reports" element={<Reports />} />
         <Route path="reports/details/:id" element={<ProblemDetails />} />
+        <Route path="homepage/details/:id" element={<ProblemDetails />} />
       </Routes>
     </>
   );
